c8d1bb243b22f4813d1335d8933ac6b572ee2982
At9-kanneki <53636695+At9-kanneki@users.noreply.github.com>
2020-05-13 17:06:26 +0800

    add detail data

---
+++ app/src/main/java/com/kanneki/githubusers/ui/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.kanneki.githubusers.ui.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kanneki.githubusers.R
+
+class DetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/kanneki/githubusers/ui/main/MainActivity.kt
+++ app/src/main/java/com/kanneki/githubusers/ui/main/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.kanneki.githubusers
+package com.kanneki.githubusers.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.recyclerview.widget.GridLayoutManager
+import com.kanneki.githubusers.R
 import com.kanneki.githubusers.helper.FakeData
-import com.kanneki.githubusers.ui.main.MainRecyclerViewAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import com.kanneki.githubusers.ui.detail.DetailActivity
+import kotlinx.android.synthetic.main.activity_recyclerview.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recyclerview)
         initView()
     }
 
@@ -26,5 +28,11 @@ class MainActivity : AppCompatActivity() {
 
         val headerView = LayoutInflater.from(this).inflate(R.layout.recyclerview_main_heard, recyclerview, false)
         mAdapter.addHeaderView(headerView)
+
+        mAdapter.setOnItemClickListener { adapter, view, position ->
+
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
